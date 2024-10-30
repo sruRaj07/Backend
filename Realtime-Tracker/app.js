b44a045d05e2115e67b025cbd2ffbfb12e0dc899
sruraj <srusinha092@gmail.com>
2024-10-31 00:08:50 +0530

    real time tracking done

--- Realtime-Tracker/app.js
+++ Realtime-Tracker/app.js
@@ -1,5 +1,7 @@
+
 const express = require("express");
 const app = express();
+app.use(express.static('public')); // Ensure 'public' has script.js and other assets
 const path = require("path");
 const http = require("http"); // we need http server to run socket.io
 
@@ -15,6 +17,15 @@ app.set(express.static(path.join(__dirname, "public"))); // public folder set up
 
 // handle the unique connection reuest
 io.on("connection", function(socket){
+    // data received from socket and now send to all the frontends
+    socket.on("send-location", function(data) {
+        io.emit("receive-location", {id: socket.id, ...data});
+    });
+
+    // if the user get disconnected
+    socket.on("disconnect", function() {
+        io.emit("user-disconnected", socket.id);
+    });
     console.log("connected successfully");
 });
 app.get("/", function(req,res){
--- Realtime-Tracker/public/js/script.js
+++ Realtime-Tracker/public/js/script.js
@@ -1,3 +1,46 @@
-const socket = io();
+const socket = io(); // connection request send to backend
 
-console.log("hey");
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition(function(position) {
+        const {latitude, longitude} = position.coords;
+        socket.emit("send-location",{ latitude,longitude});
+    },
+    function(error) {
+        console.log(error);
+    },
+    {
+        enableHighAccuracy : true,
+        timeout : 5000,
+        maximumAge : 0,
+    }
+  );
+}
+
+// to get the location
+const map = L.map("map").setView([0,0], 10);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" ,{
+    attribution: "SRURAj"
+}).addTo(map)
+
+
+const markers = {};
+// get the data from backend and display to frontend
+socket.on("receive-location", function(data) {
+    const { id,latitude,longitude} = data;
+    map.setView([latitude, longitude], 16);
+    if(markers[id]){
+        markers[id].setLatLng([latitude, longitude]);
+    }
+    else{
+        markers[id] =  L.marker([latitude,longitude]).addTo(map);
+    }
+});
+
+// if the user get disconnected we are gong to remove the marker
+socket.on("user-disconnected", function(id) {
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
